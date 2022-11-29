c617fb93ddde3c0dc17db8da6f6838bd1e102f92
Loromirov <loromirov@yandex.ru>
2022-11-29 20:44:19 +0300

    Вторая задача

--- Seminar 1/Program.cs
+++ Seminar 1/Program.cs
@@ -1,9 +1,16 @@
-﻿Console.WriteLine("Введите число");
+﻿Console.WriteLine("Введите число 1");
+string input0 = Console.ReadLine();
+Console.WriteLine("Введите число 2");
+string input1 = Console.ReadLine();
 
-string numberStr = Console.ReadLine() ?? "";
+int num0 = Convert.ToInt32(input0);
+int num0 = Convert.ToInt32(input1);
 
-int number = Convert.ToInt32(numberStr);
-
-int result = number * number;
-
-Console.WriteLine($"Квадрат числа {number} = {result}");
+    if ((num1 * num1) == num0);
+{
+    Console.WriteLine($"Число {num0} является квадратом числа {num1}");
+}
+    else 
+{
+    Console.WriteLine($"Число {num0} Не является квадратом числа {num1}");
+}
