2e6b889e06f7e56e81d7d90bf52251619694c390
Loromirov <loromirov@yandex.ru>
2022-11-29 20:53:50 +0300

    третья задача не рабочий вариант

--- Seminar 1/Program.cs
+++ Seminar 1/Program.cs
@@ -1,16 +1,35 @@
-﻿Console.WriteLine("Введите число 1");
-string input0 = Console.ReadLine();
-Console.WriteLine("Введите число 2");
-string input1 = Console.ReadLine();
+﻿Console.WriteLine("Введите число");
 
-int num0 = Convert.ToInt32(input0);
-int num0 = Convert.ToInt32(input1);
+String  day = Console.ReadLine();
 
-    if ((num1 * num1) == num0);
+if (day == "1");
 {
-    Console.WriteLine($"Число {num0} является квадратом числа {num1}");
+    Console.WriteLine("Monday");
 }
-    else 
+if (day == "2");
 {
-    Console.WriteLine($"Число {num0} Не является квадратом числа {num1}");
+    Console.WriteLine("Tuesday");
+}
+
+if (day == "3");
+{
+    Console.WriteLine("Wednesday");
+}
+if (day == "4");
+{
+    Console.WriteLine("Thursday");
+}
+
+if (day == "5");
+{
+    Console.WriteLine("Friday");
+}
+if (day == "6");
+{
+    Console.WriteLine("Saturday");
+}
+
+if (day == "7");
+{
+    Console.WriteLine("Sunday");
 }
