88547c728de0d1229294a4ddf248c9a8b4c5492d
Loromirov <loromirov@yandex.ru>
2022-11-29 20:32:21 +0300

    Первая задача

---
+++ Seminar 1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+
+string numberStr = Console.ReadLine() ?? "";
+
+int number = Convert.ToInt32(numberStr);
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
