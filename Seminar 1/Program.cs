db93a4139394f022d47d8fe061bedf3c692b1df8
Loromirov <loromirov@yandex.ru>
2022-11-29 21:19:11 +0300

    четвертая задача нерабочая

--- Seminar 1/Program.cs
+++ Seminar 1/Program.cs
@@ -1,34 +1,9 @@
-﻿Console.WriteLine("Введите число");
+﻿Console.WriteLine("Введите число: ");
 
-String  day = Console.ReadLine();
-
-switch (day)
+string nfirst = Console.ReadLine() ?? "";
+int n = Convert.ToInt32(nfirst);
+n = Math.Abs(n);
+for (int i = -(n); i <= n; i++)
 {
-    case "1" : 
-    Console.WriteLine("Monday");
-    break;
-    
-    case "2" : 
-    Console.WriteLine("Tuesday");
-    break;
-    
-    case "3" : 
-    Console.WriteLine("Wednesday");
-    break;
-    
-    case "4" : 
-    Console.WriteLine("Thursday");
-    break;
-    
-    case "5" : 
-    Console.WriteLine("Friday");
-    break;
-    
-    case "6" : 
-    Console.WriteLine("Saturday");
-    break;
-    
-    case "7" : 
-    Console.WriteLine("Sunday");
-    break;
+    Console.Write(i, " ");
 }
